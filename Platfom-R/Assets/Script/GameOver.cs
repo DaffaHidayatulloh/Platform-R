92c2373e79b5d6a768b6e865ee5df8a7306299eb
Subekti <daffahidayatulah123@gmail.com>
2025-01-18 12:08:02 +0700

    Add GameOver and Finish

--- Platfom-R/Assets/Script/GameOver.cs
+++ Platfom-R/Assets/Script/GameOver.cs
@@ -2,27 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class Finish : MonoBehaviour
+using UnityEngine.UI;
+public class GameOver : MonoBehaviour
 {
-    public GameObject finishPanel; // Untuk mengambil referensi ke panel UI
-   
+    public GameObject GameOverPanel; //Untuk mengambil refrensi ke panel game over
 
     void OnTriggerEnter2D(Collider2D other)
     {
         // Mengecek apakah pemain menyentuh objek dengan tag "Finish"
         if (other.gameObject.tag == "Player")
         {
-            finishPanel.SetActive(true); // Menampilkan panel
+            GameOverPanel.SetActive(true); // Menampilkan panel
             Time.timeScale = 0f; // Menghentikan permainan
         }
     }
 
-    // Fungsi untuk restart game, dihubungkan ke tombol restart
     public void RestartGame()
     {
-        Time.timeScale = 1f; // Lanjutkan game
+        Time.timeScale = 1f;
+        GameOverPanel.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Muat ulang scene
-        finishPanel.SetActive(false);
     }
 }
